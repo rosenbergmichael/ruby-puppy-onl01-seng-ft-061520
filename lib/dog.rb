5f88f998abee12a8f638ac86b768794a6455aaa7
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-24 21:00:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
-# Add your code here
+class Dog 
+
+  @@all = []
+  
+  attr_accessor :name 
+
+  def initialize(name)
+    @name = name 
+    save  
+  end 
+  
+  def save 
+    @@all << save
+  end   
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.print_all 
+    @@all.each do |names|
+    puts names.name 
+   end 
+  end 
+
+end   
